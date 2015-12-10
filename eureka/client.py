7a418488c9b0287e434e525b3208b4a565796334
rlhotovy <ryan.hotovy@hudl.com>
2015-12-10 14:19:40 -0600

    Remove debugging code

--- eureka/client.py
+++ eureka/client.py
@@ -211,7 +211,6 @@ class EurekaClient(object):
     def _get_from_any_instance(self, endpoint):
         for eureka_url in self.eureka_urls:
             try:
-                print urljoin(eureka_url, endpoint)
                 r = requests.get(urljoin(eureka_url, endpoint), headers={'accept': 'application/json'})
                 r.raise_for_status()
                 return json.loads(r.content)
